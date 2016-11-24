3ed4f1f61ad75f1d944dc3bc8da55cf71e3ccfca
Tristan Burke <tristan_burke@berkeley.edu>
2016-11-24 00:36:21 -0500

    commit

--- Priority_points.java
+++ Priority_points.java
@@ -10,13 +10,14 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.util.PriorityQueue; 
 
 /** use a priority queque to hold points where pixels on either side differ in RGB values greatly
  *  i.e., find points that are on the border of two colors. Then, store said point in the Priority queue,
  *  with the associated value being the difference between the pixel and it's surround colors. i.e., how 
  *  much contrast at the border. This way we will select the points with the most contrast throughout the 
- *  entire picture. Lastly, we will have to add some contrainst that does not permit consecutive or extremeley
- *  close pixels as then we could end up with our 50 pixels existing right next to each other. 
+ *  entire picture. Lastly, we will have to add some constraint that does not permit consecutive or extremeley
+ *  close pixels as then we could end up with our 50 pixels existing right next to each other (like on a single boarder). 
  **/
 
 public class Priority_Points {
